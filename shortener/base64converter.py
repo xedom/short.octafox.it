66cd0a6706b1f253b3884ae4f1cfbac486784697
xedom <xedom08@gmail.com>
2019-12-30 00:55:07 +0100

    simple converter to base 64

---
+++ shortener/base64converter.py
@@ -0,0 +1,5 @@
+characters = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","_","-"]
+
+def convert(decNum,base):
+  if decNum >= base: return convert(decNum//base,base)+characters[(decNum%base)]
+  return characters[decNum]
