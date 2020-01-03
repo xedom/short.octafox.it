948402d0ff1941d3bccb767deaa12904fffcb008
xedom <xedom08@gmail.com>
2020-01-03 05:44:07 +0100

    replaced exemple url

--- shortener/forms.py
+++ shortener/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class ShortenForm(FlaskForm):
-    originalUrl = StringField('Original Url', validators=[DataRequired()], render_kw={"placeholder": "Exemple: https://www.twitch.tv/panetty"})
+    originalUrl = StringField('Original Url', validators=[DataRequired()], render_kw={"placeholder": "Exemple: https://github.com/octafox/"})
     submit = SubmitField('Shorten')
