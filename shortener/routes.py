b49ea4d6dcc05738529b949b80ba5081b29a01a6
xedom <xedom08@gmail.com>
2020-01-03 05:13:22 +0100

    added function to redirect users to original url

--- shortener/routes.py
+++ shortener/routes.py
@@ -1,19 +1,23 @@
-from flask import render_template, flash, redirect
-from shortener import shortener
-from shortener.forms import ShortenForm
-import requests
 import random
 import re
+import requests
+from flask import render_template, flash, redirect
+
+from shortener import shortener
+from shortener.db import getOriginalLink, getShortLink
+from shortener.forms import ShortenForm
 
 api_bgImgUrl = "https://pixabay.com/api/?key=14756162-bfe9087cd12447df5f51a5fa0&q=nuvole+scuro&lang=it&image_type=photo&orientation=horizontal"
 res = requests.get(url=api_bgImgUrl)
-randomNum = random.randint(0,len(res.json()['hits']))-1
+randomNum = random.randint(0, len(res.json()['hits'])) - 1
 randomImg = res.json()['hits'][randomNum]['largeImageURL']
+
 
 @shortener.route('/')
 def index():
     form = ShortenForm()
-    return render_template('index.html', bgImage = randomImg, form=form)
+    return render_template('index.html', bgImage=randomImg, form=form)
+
 
 @shortener.route('/', methods=['POST'])
 def shorten():
@@ -22,7 +26,21 @@ def shorten():
     pattern = re.compile("(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$")
 
     if pattern.match(url):
-        return render_template('index.html', bgImage = randomImg, form=form, shortenedLink="https://short.octafox.it/testtest")
+        link = getShortLink(url)
+        short_url = "https://short.octafox.it/%s" % (link["id"])
+
+        return render_template(
+            'index.html',
+            bgImage=randomImg,
+            form=form,
+            shortenedLink=short_url
+        )
 
     flash('Invalid URL')
     return redirect('/')
+
+
+@shortener.route('/<idurl>')
+def redirect_to_url(idurl):
+    original_link = getOriginalLink(idurl)
+    return redirect(original_link["original"])
